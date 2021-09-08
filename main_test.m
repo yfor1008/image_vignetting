b035af71c32d81f87adae406ec0ed54640dd6b44
yfor <yfor1008@gmail.com>
2021-09-08 22:06:56 +0800

    [feat] update entropy test

--- main_test.m
+++ main_test.m
@@ -5,24 +5,78 @@ close all; clear; clc;
 
 fig = figure('NumberTitle', 'off', 'Name', '标题');
 x = normrnd(30, 5, [500, 500]);
-y = normrnd(400, 5, [500, 500]);
+y = normrnd(100, 5, [500, 500]);
 % x = round(x); 这里不能取整
 
-scaler = 1:15;
-for s = scaler
-    x1 = round(cat(2, x * s, y));
+% % 缩放
+% scaler = 1:0.5:4;
+% for s = scaler
+%     x1 = round(cat(2, x * s, y));
+%     [x_hist, bins] = histcounts(x1, 'BinMethod', 'integers');
+%     bins = round(bins);
+%     bins = bins(1:end-1);
+%     bar(bins, x_hist, 'FaceColor', 'b', 'EdgeColor', 'b', 'BarWidth', 1.0)
+%     xlim([0, 150])
+%     % ylim([0, 20000]);
+%     set(gcf, 'color', 'white');
+%     set(gca, 'color', 'white');
+%     set(gca, 'FontName', 'Helvetica');
+%     set(gca, 'FontSize', 13);
+%     set(gca, 'linewidth', 1.3);
+%     E = entropy(x_hist);
+%     test = 0;
+% end
+
+% % 平移
+% shift = 0:10:120;
+% for s = shift
+%     x1 = round(cat(2, x + s, y));
+%     [x_hist, bins] = histcounts(x1, 'BinMethod', 'integers');
+%     bins = round(bins);
+%     bins = bins(1:end-1);
+%     bar(bins, x_hist, 'FaceColor', 'b', 'EdgeColor', 'b', 'BarWidth', 1.0)
+%     xlim([0, 150])
+%     % ylim([0, 20000]);
+%     set(gcf, 'color', 'white');
+%     set(gca, 'color', 'white');
+%     set(gca, 'FontName', 'Helvetica');
+%     set(gca, 'FontSize', 13);
+%     set(gca, 'linewidth', 1.3);
+%     E = entropy(x_hist);
+%     test = 0;
+% end
+
+% % 缩放+log
+% scaler = 1:0.5:4;
+% for s = scaler
+%     x1 = round(cat(2, x * s, y));
+%     x1 = log(x1+1);
+%     [x_hist, bins] = histcounts(x1, 'BinMethod', 'integers');
+%     bins = round(bins);
+%     bins = bins(1:end-1);
+%     bar(bins, x_hist, 'FaceColor', 'b', 'EdgeColor', 'b', 'BarWidth', 1.0)
+%     xlim([0, 10])
+%     % ylim([0, 20000]);
+%     set(gcf, 'color', 'white');
+%     set(gca, 'color', 'white');
+%     set(gca, 'FontName', 'Helvetica');
+%     set(gca, 'FontSize', 13);
+%     set(gca, 'linewidth', 1.3);
+%     E = entropy(x_hist);
+%     test = 0;
+% end
+
+% 平移+log
+shift = 1:0.5:4;
+for s = shift
+    x1 = round(cat(2, x + s, y));
+    x1 = log(x1+1);
     [x_hist, bins] = histcounts(x1, 'BinMethod', 'integers');
     bins = round(bins);
     bins = bins(1:end-1);
     bar(bins, x_hist, 'FaceColor', 'b', 'EdgeColor', 'b', 'BarWidth', 1.0)
-    xlim([0, 500])
-    ylim([0, 20000]);
-    set(gcf, 'color', 'white');
-    set(gca, 'color', 'white');
-    set(gca, 'FontName', 'Helvetica');
-    set(gca, 'FontSize', 13);
-    set(gca, 'linewidth', 1.3);
-    
+    xlim([0, 10])
+    % ylim([0, 20000]);
     set(gcf, 'color', 'white');
     set(gca, 'color', 'white');
     set(gca, 'FontName', 'Helvetica');
@@ -31,4 +85,5 @@ for s = scaler
     E = entropy(x_hist);
     test = 0;
 end
+
 
